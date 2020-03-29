db2340f61232e717573683b39ff6d0c6e9fde8a0
Dami Tugbobo <dami@tugbobo.com>
2020-03-29 07:27:15 -0400

    Initial Commit

---
+++ ContosoUniversity/Data/SchoolContext.cs
@@ -0,0 +1,51 @@
+﻿
+using ContosoUniversity.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContosoUniversity.Data
+{
+    public class SchoolContext : DbContext
+    {
+        public SchoolContext(DbContextOptions<SchoolContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<Instructor> Instructors { get; set; }
+        public DbSet<OfficeAssignment> OfficeAssignments { get; set; }
+        public DbSet<CourseAssignment> CourseAssignments { get; set; }
+        public DbSet<Person> People { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Course>().ToTable("Course");
+            modelBuilder.Entity<Enrollment>().ToTable("Enrollment");
+            // modelBuilder.Entity<Student>().ToTable("Student");
+            modelBuilder.Entity<Person>().ToTable("Person");
+            modelBuilder.Entity<Department>().HasOne(d => d.Administrator).WithMany().OnDelete(DeleteBehavior.Restrict);
+            modelBuilder.Entity<Department>().ToTable("Department");
+            modelBuilder.Entity<Department>().Property(p => p.RowVersion).IsConcurrencyToken();
+            //modelBuilder.Entity<Instructor>().ToTable("Instructor");
+            modelBuilder.Entity<OfficeAssignment>().ToTable("OfficeAssignment");
+            modelBuilder.Entity<CourseAssignment>().ToTable("CourseAssignment");
+            modelBuilder.Entity<CourseAssignment>().HasKey(c => new { c.CourseID, c.InstructorID });
+        }
+    }
+    public static class ModelBuilderExtensions
+    {
+        public static void RemovePluralizingTableNameConvention(this ModelBuilder modelBuilder)
+        {
+            foreach (var entity in modelBuilder.Model.GetEntityTypes())
+            {
+                if (entity.BaseType == null)
+                {
+                    entity.SetTableName(entity.DisplayName());
+                }
+            }
+        }
+    }
+}
